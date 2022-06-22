24721486725a2dbd865e7417ebb7e769b6e50440
zelimran <elimrani.z@gmail.com>
2022-06-22 15:50:07 +0100

    update footer and data not found styling

--- src/app/modules/product/components/list-product/product.component.ts
+++ src/app/modules/product/components/list-product/product.component.ts
@@ -12,15 +12,25 @@ export class ProductComponent implements OnInit {
 
   showDeleteModal: boolean = false;
   products: Product[] = [];
-  show: boolean = false;
+  loading: boolean = false;
+  show: boolean = true;
+  
 
   constructor(private productService: ProductService, private notifierServire: NotifierService) { }
 
   ngOnInit(): void {
+    this.loading = true;
     this.productService.getAll().subscribe(
-      res => this.products = res, 
-      err => this.notifierServire.showNotification(err));
+      res =>{
+        this.products = res;
+        this.loading = false;
+      },
+      err => {
+        this.notifierServire.showNotification(err);
+        this.loading = false;
+      });
   }
+
   bulk(e: any) {
     if (e.target.checked == true) {
       this.checks = true;
