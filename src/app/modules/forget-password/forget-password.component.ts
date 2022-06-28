0709b67f51e0a96bebed1a2bbae1360e9da97eb2
Mohamed Jamal <medjamal.ouazani@gmail.com>
2022-06-28 21:33:13 +0100

    Adding forget password form validation

--- src/app/modules/forget-password/forget-password.component.ts
+++ src/app/modules/forget-password/forget-password.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-forget-password',
-  templateUrl: './forget-password.component.html',
-  styleUrls: ['./forget-password.component.scss']
+    selector: 'app-forget-password',
+    templateUrl: './forget-password.component.html',
+    styleUrls: ['./forget-password.component.scss']
 })
 export class ForgetPasswordComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    
+    forgetPasswordForm = new FormGroup({
+        email: new FormControl(null, [Validators.required, Validators.email])
+    });
+    
+    constructor() {
+        //
+    }
+    
+    ngOnInit(): void {
+    }
+    
+    public get forgetPasswordFormValidator() : any {
+        return this.forgetPasswordForm.controls;
+    }
 
 }
