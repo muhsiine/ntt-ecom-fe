0ddc349edd4966b060d800f0a5aac705c0cb488c
zelimran <elimrani.z@gmail.com>
2022-07-03 13:49:09 +0100

    Chnaging import method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
-import { HomeModule } from './modules/home/home.module';
-import { SharedModule } from './components/shared/shared.module';
+import { HomeModule } from '@modules/home/home.module';
+import { SharedModule } from '@components/shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './modules/login/login.component';
-import { RegisterComponent } from './modules/register/register.component';
-import { ForgetPasswordComponent } from './modules/forget-password/forget-password.component';
-import { GlobalComponent } from './components/auth-global/global.component';
-import { NotificationComponent } from './components/notification/notification.component';
-import { NotFoundComponent } from './components/error/not-found/not-found.component';
-import { NotPermitedComponent } from './components/error/not-permited/not-permited.component';
-import { HomeTemplateComponent } from './components/home-template/home-template.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from '@modules/login/login.component';
+import { RegisterComponent } from '@modules/register/register.component';
+import { ForgetPasswordComponent } from '@modules/forget-password/forget-password.component';
+import { GlobalComponent } from '@components/auth-global/global.component';
+import { NotificationComponent } from '@components/notification/notification.component';
+import { NotFoundComponent } from '@components/error/not-found/not-found.component';
+import { NotPermitedComponent } from '@components/error/not-permited/not-permited.component';
+import { HomeTemplateComponent } from '@components/home-template/home-template.component';
+import { FooterComponent } from '@components/shared/footer/footer.component';
+import { NavbarComponent } from '@components/navbar/navbar.component';
+import { SidebarComponent } from '@components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
